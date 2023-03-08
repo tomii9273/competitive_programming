18ba17741b9c0c4fa907a966d1f91912722ec334
Tomii9273 <tomii6614@yahoo.co.jp>
2023-03-09 07:39:49 +0900

    add one_indexed

--- library/make_random_tree.py
+++ library/make_random_tree.py
@@ -1,15 +1,19 @@
-# n頂点の木をランダムに作成し、辺のリスト(1-indexed)を返す。
-def make_random_tree(n):
+def make_random_tree(n, one_indexed=False):
+    """n 頂点の木をランダムに作成し、辺のリストを返す。"""
     from random import randint, shuffle
 
     A = []
     for i in range(n - 1):
-        A.append([randint(1, i + 1), i + 2])
+        A.append([randint(0, i), i + 1])
     B = [i for i in range(n)]
     shuffle(B)
     for i in range(n - 1):
         for j in range(2):
-            A[i][j] = B[A[i][j] - 1] + 1
+            A[i][j] = B[A[i][j]]
         shuffle(A[i])
     shuffle(A)
+    if one_indexed:
+        for i in range(len(A)):
+            for j in range(2):
+                A[i][j] += 1
     return A
