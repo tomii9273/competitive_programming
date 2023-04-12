c50ed2adeff1b5c408842da513ae4e4a945928d6
Tomii9273 <tomii6614@yahoo.co.jp>
2023-04-12 16:45:05 +0900

    align notation

--- library/segment_tree.py
+++ library/segment_tree.py
@@ -1,40 +1,46 @@
-# 定数倍でTLEすることがある。必要に応じてACLのsegtreeと使い分けるとよい。
+# 以下の e, op は、区間最小値を求めるようなセグメント木を作る場合の例。
+
+
+def op(x0, x1):
+    # 木に入っているもの x0, x1 の演算を返すようにする (演算は結合法則を満たす必要があるが、可換でなくてもよい) 。
+    return min(x0, x1)
 
 
 def e():
-    # 単位元を返すようにする。以下は最小値を求める場合の例。
+    # op の単位元を返す。
     return float("inf")
-
-
-def op(a, b):
-    # 木に入っているもの a, b の演算を返すようにする (演算は結合法則を満たす必要があるが、可換でなくてもよい) 。以下は最小値を求める場合の例。
-    return min(a, b)
 
 
 class Segtree:
     def __init__(self, n):
-        # sizeは最初に定めたサイズ、size2は2の累乗の値をとるサイズ(>=size)
+        # size は最初に定めたサイズ、size2 は 2 の累乗の値をとるサイズ (>=size)
         self.size = n
         i = 1
         while i < n:
             i *= 2
-        self.tree = [e() for _ in range(2 * i - 1)]
+        self.e = e()
+        self.tree = [self.e for _ in range(2 * i - 1)]
         self.size2 = i
-        self.f = e()
 
-    def __getitem__(self, i):  # [i]でi番目の値を得られるようにした
+    def __getitem__(self, i):
+        """[i] で i 番目の値を得られる。O(log n)"""
         if i < 0:
             i %= self.size
-        return self.op(i, i + 1)
+        return self.prod(i, i + 1)
 
-    def update(self, i, x):  # i番目の値をxに更新
+    def update(self, i, x):
+        """i 番目の値を x に更新。O(log n)"""
         j = self.size2 + i - 1
         self.tree[j] = x
         while j > 0:
             j = (j - 1) // 2
             self.tree[j] = op(self.tree[2 * j + 1], self.tree[2 * j + 2])
 
-    def op(self, a, b, k=0, ll=0, rr=None):  # 区間[a, b)の演算結果を返す
+    def prod(self, a, b, k=0, ll=0, rr=None):
+        """
+        区間 [a, b) の演算結果を返す。O(log n)
+        k は今見ている内部 index の番号、[ll, rr) は self.tree[k] が表す区間。
+        """
         if rr is None:
             rr = self.size2
         if rr <= a or b <= ll:
@@ -42,8 +48,8 @@ class Segtree:
         elif a <= ll and rr <= b:
             return self.tree[k]
         else:
-            vl = self.op(a, b, 2 * k + 1, ll, (ll + rr) // 2)
-            vr = self.op(a, b, 2 * k + 2, (ll + rr) // 2, rr)
+            vl = self.prod(a, b, 2 * k + 1, ll, (ll + rr) // 2)
+            vr = self.prod(a, b, 2 * k + 2, (ll + rr) // 2, rr)
             return op(vl, vr)
 
 
